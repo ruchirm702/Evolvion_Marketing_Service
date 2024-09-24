397c9b7129c597227a392967e0dfb8d0b017c737
Ruchir Mandora <ruchirm702@gmail.com>
2024-09-24 23:51:18 +0530

    Initial commit with base project setup

---
+++ src/main/java/dev/ruchir/evolvion_marketing_service/EvolvionMarketingServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_marketing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolvionMarketingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvolvionMarketingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruchir/evolvion_marketing_service/EvolvionMarketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruchir.evolvion_marketing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolvionMarketingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
